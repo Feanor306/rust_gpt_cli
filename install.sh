854cf622cab1e5c2dee46b57b84aa95477dacb7e
George Parsehyan <feanor306@live.com>
2023-01-20 20:44:47 +0100

    added install script and updated README

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Collection of build, install, run commands
+# requires cargo and rustc
+
+# builds the binary to /target/release/rust_gpt_cli
+cargo build --release
+
+# Install in specific location (optional)
+# cp /target/release/rust_gpt_cli /opt/rust_gpt_cli/rust_gpt_cli
+
+# Set ENV variables before running 
+# export OPENAI_API_KEY="key" # (mandatory)
+# export OPENAI_MAX_TOKENS="1000" # (optional, default 1000)
+
+# Run with cargo
+# cargo run --release
+
+# Run binary
+# ./target/release/rust_gpt_cli
